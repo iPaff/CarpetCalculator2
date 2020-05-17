0398396fa0c2bd6b7931205f9fba7018db76a600
Pavel Kalachev <pavelkalachev@gmail.com>
2020-05-17 21:11:12 +0300

    Initial commit

---
+++ src/com/company/Floor.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Floor {
+    private double width;
+    private double length;
+
+    public Floor(double width, double length) {
+        this.width = width;
+        this.length = length;
+        if (width < 0){
+            this.width = 0;
+        }else if(length < 0){
+            this.length = 0;
+        }
+    }
+    public double getArea(){
+        return width*length;
+    }
+}
